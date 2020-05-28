63189866b95c7dc846c4f22b56b066e4b986c19f
prerna <bhatraprerna061@gmail.com>
2020-05-28 10:09:29 +0530

    pagination

--- head.php
+++ head.php
@@ -4,6 +4,7 @@
 	  <meta name="viewport" content="width=device-width, initial-scale=1">
 	  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
 	  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.21/css/jquery.dataTables.min.css">
+	  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	  <link rel="stylesheet" type="text/css" href="css/dtable.css">
 	  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 	  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <!DOCTYPE html>
 <html>
 <?php
-require('head.php')
+require('head.php');
+require('function.php')
+
 ?>
 <body>
 	<div class="container">
@@ -10,41 +12,37 @@ require('head.php')
         <thead>
             <tr>
                 <th>Name</th>
+                <th>Email</th>
+                <th>Phone Number</th>
+                <th>DOB</th>
+                <th>Action</th>
             </tr>
         </thead>
         <tbody>
-            <tr>
-                <td>Tiger Nixon</td>
-             
-            </tr>
-            <tr>
-                <td>Garrett Winters</td>
-                
-            </tr>
-            <tr>
-                <td>Ashton Cox</td>
-               
-            </tr>
-            <tr>
-                <td>Cedric Kelly</td>
-                
-            </tr>
-            <tr>
-                <td>Airi Satou</td>
-              
-            </tr>
-            <tr>
-                <td>Brielle Williamson</td>
-                
-            </tr>
-            <tr>
-                <td>Herrod Chandler</td>
-               
-            </tr>
-            
+            <?php
+                $fetchdata=new DB_con();
+                    $sql=$fetchdata->fetchdata();
+                     $cnt=1;
+                      while($row=mysqli_fetch_array($sql))
+                      {
+                      ?>
+                    <tr>
+                        <td><?php echo $row['FirstName']?></td>
+                        <td><?php echo $row['EmailId']?></td>
+                        <td><?php echo $row['ContactNumber']?></td>
+                        <td><?php echo $row['DOB']?></td>
+                        <td><a href="#"><abbr title="Edit"><i class="fa fa-pencil "></i></abbr></a>
+                            <a id="trash"href="#"><abbr title="Delete"><i  class="fa fa-trash "></i></abbr></a></td>
+
+                    </tr>    
+                    <?php
+                }
+                    ?>
         </tbody>
     </table>
-	 <a href="insert.php" >ADD New</a>
+    <div class="add" style="margin-top: 20px;">
+        <a href="insert.php" class="btn btn-info" >ADD New</a>
+    </div>
 </div>
 </body>
 </html>
